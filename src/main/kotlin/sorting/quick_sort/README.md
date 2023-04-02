06cdadb64fe9e085b4ca5d851307bc2bbc9eb116
kaikeventura <kaike.gnr@hotmail.com>
2023-04-02 19:16:53 -0300

    Quick sort algorithm

---
+++ src/main/kotlin/sorting/quick_sort/README.md
@@ -0,0 +1,7 @@
+O algoritmo Quick Sort é amplamente utilizado em situações em que é necessário ordenar um grande conjunto de dados de forma eficiente. Ele é um dos algoritmos de ordenação mais rápidos para conjuntos de dados grandes e pode ser utilizado em diversas aplicações.
+
+No entanto, é importante destacar que o Quick Sort não é recomendado para conjuntos de dados muito pequenos, uma vez que o tempo de execução do algoritmo pode se tornar menos eficiente do que outros algoritmos de ordenação mais simples, como o Bubble Sort ou o Insertion Sort.
+
+Em geral, o Quick Sort é uma boa escolha para ordenar conjuntos de dados grandes e aleatórios, mas é necessário tomar cuidado ao utilizá-lo em outras situações, como conjuntos de dados já parcialmente ordenados, por exemplo. Nesses casos, outros algoritmos de ordenação podem ser mais eficientes.
+
+By: ChatGPT
---
+++ src/main/kotlin/sorting/quick_sort/Run.kt
@@ -0,0 +1,35 @@
+package sorting.quick_sort
+
+fun main() {
+    val numbers = intArrayOf(5, 3, 8, 4, 2)
+    quickSort(numbers, 0, numbers.size - 1)
+    numbers.forEach { println(it) }
+}
+
+fun quickSort(numbers: IntArray, start: Int, end: Int) {
+    if (start < end) {
+        val partitionIndex = partition(numbers, start, end)
+        quickSort(numbers, start, partitionIndex - 1)
+        quickSort(numbers, partitionIndex + 1, end)
+    }
+}
+
+fun partition(numbers: IntArray, start: Int, end: Int): Int {
+    val pivot = numbers[end]
+    var i = start - 1
+
+    for (j in start until end) {
+        if (numbers[j] <= pivot) {
+            i++
+            val temp = numbers[i]
+            numbers[i] = numbers[j]
+            numbers[j] = temp
+        }
+    }
+
+    val temp = numbers[i + 1]
+    numbers[i + 1] = numbers[end]
+    numbers[end] = temp
+
+    return i + 1
+}
