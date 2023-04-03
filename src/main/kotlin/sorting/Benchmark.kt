0a2b1f383b6bbc8ad5b3f861cb161eee03f8428d
kaikeventura <kaike.gnr@hotmail.com>
2023-04-02 22:16:04 -0300

    Criado benchmark para comparar a execução dos algoritmos com listas de tamanhos diferentes

---
+++ src/main/kotlin/sorting/Benchmark.kt
@@ -0,0 +1,30 @@
+package sorting
+
+import sorting.bubble_short.bubbleSort
+import sorting.merge_short.mergeSort
+import sorting.quick_sort.quickSort
+import java.util.Random
+import kotlin.system.measureNanoTime
+
+fun main() {
+    val shortArrayNumbers = randomNumbers(10)
+    val performanceTimeAlgorithmsWithShotList = mutableMapOf(
+        "Bubble sort with a short list" to shortArrayNumbers.clone().let { measureNanoTime { bubbleSort(it) } },
+        "Merge sort with a short list" to shortArrayNumbers.clone().let { measureNanoTime { mergeSort(it) } },
+        "Quick sort with a short list" to shortArrayNumbers.clone().let { measureNanoTime { quickSort(it, 0, it.size - 1) } }
+    )
+    performanceTimeAlgorithmsWithShotList.toList().sortedBy { (_, value) -> value }.toMap().forEach { println(it) }
+
+    val bigArrayNumbers = randomNumbers(10000)
+    val performanceTimeAlgorithmsWithBigList = mutableMapOf(
+        "Bubble sort with a big list" to bigArrayNumbers.clone().let { measureNanoTime { bubbleSort(it) } },
+        "Merge sort with a big list" to bigArrayNumbers.clone().let { measureNanoTime { mergeSort(it) } },
+        "Quick sort with a big list" to bigArrayNumbers.clone().let { measureNanoTime { quickSort(it, 0, it.size - 1) } }
+    )
+    performanceTimeAlgorithmsWithBigList.toList().sortedBy { (_, value) -> value }.toMap().forEach { println(it) }
+}
+
+fun randomNumbers(size: Long): IntArray =
+    Random().ints(0, 999999999)
+        .limit(size)
+        .toArray()
